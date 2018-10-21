69d71f9fef006f76b4bc3e7c75073eecf80bd011
Rohith Menon <rohith.menon@kittyhawk.aero>
2018-10-21 10:06:40 -0700

    Install g++7 and set CMake variables

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
-sudo apt-get install libuv1-dev
+sudo apt-get update && apt-get install -y \
+    libuv1-dev \
+    python-software-properties \
+    software-properties-common \
+    cmake \
+    libssl-dev
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt update && sudo apt install gcc-7 g++-7 -y
 git clone https://github.com/uWebSockets/uWebSockets 
+export CC=/usr/bin/gcc-7
+export CXX=/usr/bin/g++-7
 cd uWebSockets
 git checkout e94b6e1
 mkdir build
@@ -13,6 +22,3 @@ cd ..
 sudo ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
 sudo rm -r uWebSockets
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt update
-sudo apt install g++-7 -y
